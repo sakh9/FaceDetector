48a91d92f336690c6f528dcad4dec4bea8279287
sakha <b1nt4ngalby@gmail.com>
2026-02-03 12:40:13 +0700

    Implement camera recognition functionality with Keras model integration

---
+++ main_src/camera_recognition.py
@@ -0,0 +1,5 @@
+import main_src.main as main  # Import the main module to access its functions and variables
+
+# Start the camera recognition process
+if __name__ == "__main__":
+    main.camera_recognition()
